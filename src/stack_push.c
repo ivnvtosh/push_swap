ea8a6e1ba98334f27d48e91b4016f5946d4a7c7b
Anton <87498837+ivnvtosh@users.noreply.github.com>
2022-01-15 16:41:28 +0300

    stack_push

--- src/stack_push.c
+++ src/stack_push.c
@@ -13,39 +13,42 @@
 #include "../libft/libft.h"
 #include "push_swap.h"
 
-void	stack_push(t_stack **stack_from, t_stack **stack_in)
+void	disconnect(t_stack **from)
 {
-	t_stack	*elem;
-
-	elem = *stack_from;
-	if (elem == NULL)
-		return ;
-	if (elem == elem->next)
-		*stack_from = NULL;
+	if (*from == (*from)->next)
+		*from = NULL;
 	else
 	{
-		(*stack_from)->next->prev = (*stack_from)->prev;
-		(*stack_from)->prev->next = (*stack_from)->next;
-		(*stack_from) = (*stack_from)->next;
+		(*from)->next->prev = (*from)->prev;
+		(*from)->prev->next = (*from)->next;
+		(*from) = (*from)->next;
 	}
-	if (*stack_in == NULL)
+}
+
+void	connect(t_stack *elem, t_stack **in)
+{
+	if (*in == NULL)
 	{
 		elem->next = elem;
 		elem->prev = elem;
 	}
-	else if ((*stack_in)->next == NULL)
-	{
-		elem->next = *stack_in;
-		elem->prev = *stack_in;
-		(*stack_in)->next = elem;
-		(*stack_in)->prev = elem;
-	}
 	else
 	{
-		elem->next = *stack_in;
-		elem->prev = (*stack_in)->prev;
-		(*stack_in)->prev->next = elem;
-		(*stack_in)->prev = elem;
+		elem->next = *in;
+		elem->prev = (*in)->prev;
+		(*in)->prev->next = elem;
+		(*in)->prev = elem;
 	}
-	*stack_in = elem;
+	*in = elem;
+}
+
+void	stack_push(t_stack **from, t_stack **in)
+{
+	t_stack	*elem;
+
+	elem = *from;
+	if (elem == NULL)
+		return ;
+	disconnect(from);
+	connect(elem, in);
 }
