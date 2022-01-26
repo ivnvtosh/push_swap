64ce6dc8b9b011720629db526988e31ee37bd208
Anton <87498837+ivnvtosh@users.noreply.github.com>
2022-01-26 16:16:06 +0300

    sort

--- src/sort.c
+++ src/sort.c
@@ -1,29 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   sort.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ccamie <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/25 18:14:25 by ccamie            #+#    #+#             */
-/*   Updated: 2021/12/25 18:14:28 by ccamie           ###   ########.fr       */
+/*   Created: 2022/01/26 16:12:27  by ccamie            #+#    #+#             */
+/*   Updated: 2022/01/26 16:12:35 by ccamie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/libft.h"
 #include "push_swap.h"
 
-t_stack	*parser(int *count, char **numbers);
-void	sort(t_stack **stack_a, t_stack **stack_b, int count);
-void	terminate(t_stack *stack_a, t_stack *stack_b, int code);
+void	quicksort(t_stack **stack_a, t_stack **stack_b, int count);
+void	print(t_stack *stack_a, t_stack *stack_b);
 
-void	push_swap(int count, char **numbers)
+void	sort(t_stack **stack_a, t_stack **stack_b, int count)
 {
-	t_stack	*stack_a;
-	t_stack	*stack_b;
-
-	stack_a = parser(&count, numbers);
-	stack_b = NULL;
-	sort(&stack_a, &stack_b, count);
-	terminate(stack_a, stack_b, 0);
+	quicksort(stack_a, stack_b, count);
 }
